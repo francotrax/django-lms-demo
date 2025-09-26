dd9d66b738bca76d6de51e4ba8f2a4e67f4d321c
Franjo Pesut <franjo95@gmail.com>
2025-09-26 10:23:34 +0200

    initial

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&@8^s=_%^0r^4&2iw(eg4yopx+8yy@-=!7+l1at-m&sg$p!$mm"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
